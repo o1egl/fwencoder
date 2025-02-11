edda7e7ee82407842a04e9f9f0be92abcb8dd3f2
Oleg Lobanov <oleg.lobanov@bitvavo.com>
2025-02-11 16:33:51 +0100

    Upgrade to go 1.23

--- decoder.go
+++ decoder.go
@@ -10,7 +10,6 @@ import (
 	"reflect"
 	"regexp"
 	"runtime"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -51,14 +50,12 @@ var (
 //	    BDate    time.Time `column:"Birthday" format:"2006/01/02"`
 //	    Postcode int       `json:"Zip"`
 //	}
-func Unmarshal(data []byte, v interface{}) error {
+func Unmarshal(data []byte, v any) error {
 	return UnmarshalReader(bytes.NewReader(data), v)
 }
 
 // UnmarshalReader behaves the same as Unmarshal, but reads data from io.Reader
-//
-//nolint:gocyclo
-func UnmarshalReader(reader io.Reader, v interface{}) (err error) {
+func UnmarshalReader(reader io.Reader, v any) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			if _, ok := r.(runtime.Error); ok {
@@ -68,45 +65,49 @@ func UnmarshalReader(reader io.Reader, v interface{}) (err error) {
 		}
 	}()
 
-	sliceItemType := reflect.TypeOf(v)
-	if sliceItemType != nil && sliceItemType.Kind() == reflect.Ptr {
-		sliceItemType = sliceItemType.Elem()
-	} else {
-		return ErrIncorrectInputValue
+	sliceItemType, isSliceItemPtr, err := validateInput(v)
+	if err != nil {
+		return err
 	}
 
-	if sliceItemType.Kind() == reflect.Slice {
-		sliceItemType = sliceItemType.Elem()
-	} else {
-		return ErrIncorrectInputValue
-	}
-
-	slice := reflect.ValueOf(v)
-	if slice.Kind() == reflect.Ptr {
-		slice = slice.Elem()
-	}
-
+	slice := reflect.ValueOf(v).Elem()
 	slice.Set(slice.Slice(0, 0))
 
-	sliceType := sliceItemType
-	if sliceType.Kind() == reflect.Ptr {
-		sliceType = sliceType.Elem()
+	return parseData(reader, slice, sliceItemType, isSliceItemPtr)
+}
+
+func validateInput(v any) (sliceItemType reflect.Type, isSliceItemPtr bool, err error) {
+	sliceItemType = reflect.TypeOf(v)
+	if sliceItemType == nil || sliceItemType.Kind() != reflect.Ptr {
+		return nil, false, ErrIncorrectInputValue
+	}
+	sliceItemType = sliceItemType.Elem()
+
+	if sliceItemType.Kind() != reflect.Slice {
+		return nil, false, ErrIncorrectInputValue
+	}
+	sliceItemType = sliceItemType.Elem()
+
+	isSliceItemPtr = sliceItemType.Kind() == reflect.Ptr
+	if isSliceItemPtr {
+		sliceItemType = sliceItemType.Elem()
 	}
 
-	if sliceType.Kind() != reflect.Struct {
-		return ErrIncorrectInputValue
+	if sliceItemType.Kind() != reflect.Struct {
+		return nil, false, ErrIncorrectInputValue
 	}
 
+	return sliceItemType, isSliceItemPtr, nil
+}
+
+func parseData(reader io.Reader, slice reflect.Value, sliceItemType reflect.Type, isSliceItemPtr bool) error {
 	scanner := bufio.NewScanner(reader)
-	columnNames := getColumns(sliceType)
-	sort.Slice(columnNames, func(i, j int) bool {
-		return len([]rune(columnNames[i])) > len([]rune(columnNames[j]))
-	})
 	fieldsIndex := make(map[string]string)
 	isHeaderParsed := false
 	lineNum := 0
 	headersLength := 0
-	columns := make([]fwColumn, 0, len(columnNames))
+	columnNames := getColumns(sliceItemType)
+	var columns []fwColumn
 
 	for scanner.Scan() {
 		lineNum++
@@ -115,6 +116,7 @@ func UnmarshalReader(reader io.Reader, v interface{}) (err error) {
 		if !isHeaderParsed {
 			isHeaderParsed = true
 			headersLength = len(lineRunes)
+			var err error
 			columns, err = parseHeaders(line, columnNames)
 			if err != nil {
 				return err
@@ -129,11 +131,11 @@ func UnmarshalReader(reader io.Reader, v interface{}) (err error) {
 			fieldsIndex[prnColumn.name] = string(lineRunes[prnColumn.start:prnColumn.end])
 		}
 
-		newItem, err := createObject(fieldsIndex, sliceType)
+		newItem, err := createObject(fieldsIndex, sliceItemType)
 		if err != nil {
 			return fmt.Errorf("error in line %d: %w", lineNum, err)
 		}
-		if sliceItemType.Kind() != reflect.Ptr {
+		if !isSliceItemPtr {
 			newItem = newItem.Elem()
 		}
 		slice.Set(reflect.Append(slice, newItem))
@@ -142,7 +144,7 @@ func UnmarshalReader(reader io.Reader, v interface{}) (err error) {
 	return nil
 }
 
-func getRefName(field reflect.StructField) string {
+func getRefName(field *reflect.StructField) string {
 	if name, ok := field.Tag.Lookup(columnTagName); ok {
 		return name
 	}
@@ -156,118 +158,43 @@ func createObject(fieldsIndex map[string]string, t reflect.Type) (reflect.Value,
 	sp := reflect.New(t)
 	s := sp.Elem()
 	fieldsCount := s.NumField()
-	for fieldIndex := 0; fieldIndex < fieldsCount; fieldIndex++ {
+	for fieldIndex := range fieldsCount {
 		currentField := s.Field(fieldIndex)
 		typeField := s.Type().Field(fieldIndex)
-		refName := getRefName(typeField)
+		refName := getRefName(&typeField)
 
 		rawValue, ok := fieldsIndex[refName]
 		if !ok {
 			continue
 		}
-		if err := setFieldValue(currentField, typeField, rawValue); err != nil {
+		if err := setFieldValue(currentField, &typeField, rawValue); err != nil {
 			return s, err
 		}
 	}
 	return sp, nil
 }
 
-//nolint:gocyclo,funlen
-func setFieldValue(field reflect.Value, structField reflect.StructField, rawValue string) error {
+func setFieldValue(field reflect.Value, structField *reflect.StructField, rawValue string) error {
 	rawValue = strings.TrimSpace(rawValue)
 	fieldKind := field.Type().Kind()
 	isPointer := fieldKind == reflect.Ptr
 	if isPointer {
 		fieldKind = field.Type().Elem().Kind()
 	}
-	//nolint:dupl
 	switch fieldKind {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		value, err := strconv.ParseInt(rawValue, 10, 0)
-		if err != nil {
-			return newCastingError(err, rawValue, structField)
-		}
-		if isPointer {
-			v := reflect.New(field.Type().Elem())
-			if v.Elem().OverflowInt(value) {
-				return newOverflowError(value, structField)
-			}
-			v.Elem().SetInt(value)
-			field.Set(v)
-		} else {
-			if field.OverflowInt(value) {
-				return newOverflowError(value, structField)
-			}
-			field.SetInt(value)
-		}
+		return setIntFieldValue(field, structField, rawValue, isPointer)
 	case reflect.Float32, reflect.Float64:
-		value, err := strconv.ParseFloat(rawValue, 64)
-		if err != nil {
-			return newCastingError(err, rawValue, structField)
-		}
-		if isPointer {
-			v := reflect.New(field.Type().Elem())
-			if v.Elem().OverflowFloat(value) {
-				return newOverflowError(value, structField)
-			}
-			v.Elem().SetFloat(value)
-			field.Set(v)
-		} else {
-			if field.OverflowFloat(value) {
-				return newOverflowError(value, structField)
-			}
-			field.SetFloat(value)
-		}
+		return setFloatFieldValue(field, structField, rawValue, isPointer)
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		value, err := strconv.ParseUint(rawValue, 10, 64)
-		if err != nil {
-			return newCastingError(err, rawValue, structField)
-		}
-		if isPointer {
-			v := reflect.New(field.Type().Elem())
-			if v.Elem().OverflowUint(value) {
-				return newOverflowError(value, structField)
-			}
-			v.Elem().SetUint(value)
-			field.Set(v)
-		} else {
-			if field.OverflowUint(value) {
-				return newOverflowError(value, structField)
-			}
-			field.SetUint(value)
-		}
+		return setUintFieldValue(field, structField, rawValue, isPointer)
 	case reflect.String:
-		if isPointer {
-			field.Set(reflect.ValueOf(&rawValue))
-		} else {
-			field.SetString(rawValue)
-		}
+		return setStringFieldValue(field, rawValue, isPointer)
 	case reflect.Bool:
-		value, err := strconv.ParseBool(rawValue)
-		if err != nil {
-			return newCastingError(err, rawValue, structField)
-		}
-		if isPointer {
-			field.Set(reflect.ValueOf(&value))
-		} else {
-			field.SetBool(value)
-		}
+		return setBoolFieldValue(field, structField, rawValue, isPointer)
 	case reflect.Struct:
 		if field.Type() == reflect.TypeOf(time.Time{}) || field.Type() == reflect.TypeOf(&time.Time{}) {
-			timeFormat, ok := structField.Tag.Lookup(format)
-			if !ok {
-				timeFormat = time.RFC3339
-			}
-			t, err := time.Parse(timeFormat, rawValue)
-			if err != nil {
-				return newCastingError(err, rawValue, structField)
-			}
-			if isPointer {
-				field.Set(reflect.ValueOf(&t))
-			} else {
-				field.Set(reflect.ValueOf(t))
-			}
-			return nil
+			return setTimeFieldValue(field, structField, rawValue, isPointer)
 		}
 		fallthrough
 	default:
@@ -281,20 +208,124 @@ func setFieldValue(field reflect.Value, structField reflect.StructField, rawValu
 	return nil
 }
 
-func newCastingError(err error, rawValue string, structField reflect.StructField) error {
+//nolint:dupl // it's not a duplicate
+func setIntFieldValue(field reflect.Value, structField *reflect.StructField, rawValue string, isPointer bool) error {
+	value, err := strconv.ParseInt(rawValue, 10, 0)
+	if err != nil {
+		return newCastingError(err, rawValue, structField)
+	}
+	if isPointer {
+		v := reflect.New(field.Type().Elem())
+		if v.Elem().OverflowInt(value) {
+			return newOverflowError(value, structField)
+		}
+		v.Elem().SetInt(value)
+		field.Set(v)
+	} else {
+		if field.OverflowInt(value) {
+			return newOverflowError(value, structField)
+		}
+		field.SetInt(value)
+	}
+	return nil
+}
+
+func setFloatFieldValue(field reflect.Value, structField *reflect.StructField, rawValue string, isPointer bool) error {
+	value, err := strconv.ParseFloat(rawValue, 64)
+	if err != nil {
+		return newCastingError(err, rawValue, structField)
+	}
+	if isPointer {
+		v := reflect.New(field.Type().Elem())
+		if v.Elem().OverflowFloat(value) {
+			return newOverflowError(value, structField)
+		}
+		v.Elem().SetFloat(value)
+		field.Set(v)
+	} else {
+		if field.OverflowFloat(value) {
+			return newOverflowError(value, structField)
+		}
+		field.SetFloat(value)
+	}
+	return nil
+}
+
+//nolint:dupl // it's not a duplicate
+func setUintFieldValue(field reflect.Value, structField *reflect.StructField, rawValue string, isPointer bool) error {
+	value, err := strconv.ParseUint(rawValue, 10, 64)
+	if err != nil {
+		return newCastingError(err, rawValue, structField)
+	}
+	if isPointer {
+		v := reflect.New(field.Type().Elem())
+		if v.Elem().OverflowUint(value) {
+			return newOverflowError(value, structField)
+		}
+		v.Elem().SetUint(value)
+		field.Set(v)
+	} else {
+		if field.OverflowUint(value) {
+			return newOverflowError(value, structField)
+		}
+		field.SetUint(value)
+	}
+	return nil
+}
+
+func setStringFieldValue(field reflect.Value, rawValue string, isPointer bool) error {
+	if isPointer {
+		field.Set(reflect.ValueOf(&rawValue))
+	} else {
+		field.SetString(rawValue)
+	}
+	return nil
+}
+
+func setBoolFieldValue(field reflect.Value, structField *reflect.StructField, rawValue string, isPointer bool) error {
+	value, err := strconv.ParseBool(rawValue)
+	if err != nil {
+		return newCastingError(err, rawValue, structField)
+	}
+	if isPointer {
+		field.Set(reflect.ValueOf(&value))
+	} else {
+		field.SetBool(value)
+	}
+	return nil
+}
+
+func setTimeFieldValue(field reflect.Value, structField *reflect.StructField, rawValue string, isPointer bool) error {
+	timeFormat, ok := structField.Tag.Lookup(format)
+	if !ok {
+		timeFormat = time.RFC3339
+	}
+	t, err := time.Parse(timeFormat, rawValue)
+	if err != nil {
+		return newCastingError(err, rawValue, structField)
+	}
+	if isPointer {
+		field.Set(reflect.ValueOf(&t))
+	} else {
+		field.Set(reflect.ValueOf(t))
+	}
+	return nil
+}
+
+func newCastingError(err error, rawValue string, structField *reflect.StructField) error {
 	return fmt.Errorf(`filed casting "%s" to "%s:%v": %w`, rawValue, structField.Name, structField.Type, err)
 }
 
-func newOverflowError(value any, structField reflect.StructField) error {
+func newOverflowError(value any, structField *reflect.StructField) error {
 	return fmt.Errorf(`value %v is too big for field %s:%v`, value, structField.Name, structField.Type)
 }
 
 func getColumns(sType reflect.Type) []string {
 	fCount := sType.NumField()
 	columnNames := make([]string, 0, fCount)
-	for i := 0; i < fCount; i++ {
+	for i := range fCount {
 		field := sType.Field(i)
-		column := getRefName(field)
+		column := getRefName(&field)
 		columnNames = append(columnNames, column)
 	}
 	return columnNames
@@ -302,7 +333,7 @@ func getColumns(sType reflect.Type) []string {
 
 func parseHeaders(headerLine string, columnNames []string) ([]fwColumn, error) {
 	columns := make([]fwColumn, 0, len(columnNames))
-	for i := 0; i < len(columnNames); i++ {
+	for i := range columnNames {
 		colName := columnNames[i]
 		re, err := regexp.Compile(fmt.Sprintf("(%s *)", colName))
 		if err != nil {
